e5a849e1db69cb3c3e8294b2b1ea929d00a2568c
Reinoldo Canhette Junior <rcanhette@rochalog.com.br>
2022-08-05 02:29:43 -0300

    criação das classes JWTAuthenticationFilter e JWTAuthorizationFilter

---
+++ src/main/java/br/com/softblue/bluetask/infrastructure/web/security/SecurityConstants.java
@@ -0,0 +1,9 @@
+package br.com.softblue.bluetask.infrastructure.web.security;
+
+public class SecurityConstants {
+
+    public static final long EXPIRATION_TIME = 86400000; //1 dia
+    public static final String SECRET_KEY = "SaRafa123";
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+    public static final String TOKEN_PREFIX = "Beare ";
+}
